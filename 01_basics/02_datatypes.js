0ec7bed9aa6041edda5ba9d79d119cd21204bc03
Aviral7060 <110628855+Aviral7060@users.noreply.github.com>
2024-02-24 14:51:27 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; // treat all js codes with the newer version of the js 
+// alert(3+3) we are using node js not browser 
+// number 
+// string
+// null => empty
+//undefined
+// boolean
+//object => most complicated
+// symbol => used for the unique values
+console.log(typeof null);
+// very imp
+console.log(typeof undefined);
