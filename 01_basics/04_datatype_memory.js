a55422fcf722e9e3931804583711e083ae495f21
Aviral7060 <110628855+Aviral7060@users.noreply.github.com>
2024-02-25 06:49:49 +0000

    memory basic

---
+++ 01_basics/04_datatype_memory.js
@@ -0,0 +1,9 @@
+// two types of datatypes 
+// 1.) Primitive contains (number,string, symbol, Boolean,BigInt,null,undefined)
+// 2.) Non primitive or Reference (array, function, object)
+
+// memory is of Two types
+/* 1.) STACK all the primitive
+   2.) HEAP for non primitive
+   in stack we get the copy of the variable but in heap we get the reference
+   */ 
