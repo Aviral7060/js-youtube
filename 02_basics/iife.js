6bc0b4efd136b436298f67b9f500f482abb1f4c1
Aviral7060 <110628855+Aviral7060@users.noreply.github.com>
2024-04-03 17:49:46 +0000

    iife

---
+++ 02_basics/iife.js
@@ -0,0 +1,14 @@
+//()() these two brackets are function definition and funciton call
+// also called as immediate funciton call
+//global declaration ke variables hataane ke liye
+// to end this we put semi colon
+// this is a named iife
+(function chai(){
+    console.log(`i love chai`);
+})();
+
+// unnamed iife and arrow function
+
+(()=>{
+ console.log(`my name`);
+})()
